2bd9eb44625196cbfc06823800f99c93b0883592
kwentinn <kwentinn@gmail.com>
2020-02-26 00:01:17 +0100

    update validator

--- Registrations.Domain/Registrations/Commands/Validators/CreateRegistrationValidator.cs
+++ Registrations.Domain/Registrations/Commands/Validators/CreateRegistrationValidator.cs
@@ -8,27 +8,39 @@ namespace Registrations.Domain.Registrations.Commands.Validators
 {
 	public class CreateRegistrationValidator : AbstractValidator<CreateRegistration>
 	{
-		private readonly IDispatcher dispatcher;
-		public CreateRegistrationValidator()
+		private readonly IDispatcher _dispatcher;
+
+
+		public CreateRegistrationValidator(IDispatcher dispatcher)
 		{
+			_dispatcher = dispatcher;
+
 			RuleFor(c => c.Person)
 				.NotNull().WithMessage("Person is required")
-			;
+				.MustAsync(UserExistAsync).WithMessage("User does not exist");
 
 			RuleFor(c => c.RegistrationTarget)
-				.NotNull().WithMessage("Registration target is required")
-			;
+				.NotNull().WithMessage("Registration target is required");
 
 			RuleFor(c => c.RegistrationDate)
-				.NotNull().WithMessage("Registration date is required")
-			;
+				.NotNull().WithMessage("Registration date is required");
 
-			RuleFor(c => c).MustAsync(RegistrationNotExist).WithMessage("Registration already exists");
+			RuleFor(c => c)
+				.MustAsync(RegistrationNotExistAsync).WithMessage("Registration already exists");
 		}
 
-		private async Task<bool> RegistrationNotExist(CreateRegistration obj, CancellationToken cancellationToken)
+		private async Task<bool> RegistrationNotExistAsync(CreateRegistration obj, CancellationToken cancellationToken)
 		{
-			throw new NotImplementedException();
+			return await _dispatcher.GetResultAsync(new Queries.RegistrationExists
+			(
+				obj.Person.Id, 
+				obj.RegistrationTarget.Id,
+				obj.RegistrationDate
+			));
+		}
+		private async Task<bool> UserExistAsync(Person p, CancellationToken cancellationToken)
+		{
+			return await _dispatcher.GetResultAsync(new Queries.UserExistsWithEmail(p.EmailAddress));
 		}
 	}
 }
