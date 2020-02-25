8ed282dfeaa915b57607ce3ed886c0a547e1a81f
kwentinn <kwentinn@gmail.com>
2020-02-26 00:01:29 +0100

    new queries

---
+++ Registrations.Domain/Registrations/Commands/Validators/Queries/UserExistsWithEmail.cs
@@ -0,0 +1,16 @@
+﻿using Itenso.TimePeriod;
+using Kledex.Queries;
+using System;
+
+namespace Registrations.Domain.Registrations.Commands.Validators.Queries
+{
+	public class UserExistsWithEmail : IQuery<bool>
+	{
+		public string EmailAddress { get; }
+		
+		public UserExistsWithEmail(string emailAddress)
+		{
+			EmailAddress = emailAddress;
+		}
+	}
+}
---
+++ Registrations.Domain/Registrations/Commands/Validators/Queries/UserExistsWithInfo.cs
@@ -0,0 +1,19 @@
+﻿using Itenso.TimePeriod;
+using Kledex.Queries;
+
+namespace Registrations.Domain.Registrations.Commands.Validators.Queries
+{
+	public class UserExistsWithInfo : IQuery<bool>
+	{
+		public string Firstname{ get; }
+		public string Lastname{ get; }
+		public Date DateofBirth { get; }
+		
+		public UserExistsWithInfo(string firstname, string lastname, Date dateOfBirth)
+		{
+			Firstname = firstname;
+			Lastname = lastname;
+			DateofBirth = dateOfBirth;
+		}
+	}
+}
